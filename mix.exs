10cfcc6f913d1d47927695cea95a49f9fc378241
LuizFerK <luizfernandoklein@live.com>
2022-01-18 20:35:30 -0300

    chore: Absinthe

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule Nftex.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.6.0"},
+      {:absinthe_phoenix, "~> 2.0"}
     ]
   end
 
